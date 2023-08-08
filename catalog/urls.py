56da2df8fb1ac516b90eb939539d9ddc2b279df1
dariabushueva <bushueva84@yandex.ru>
2023-08-08 22:05:09 +0300

    changed views FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product, products, categories, add_product
+from catalog.views import ProductListView, contacts, ProductDetailView, ProductsListView, ProductCreateView, CategoryListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:pk>/products/', products, name='products'),
-    path('<int:pk>/product/', product, name='product'),
-    path('categories/', categories, name='categories'),
+    path('', ProductListView.as_view(), name='index'),
+    path('<int:pk>/products/', ProductsListView.as_view(), name='products'),
+    path('<int:pk>/product/', ProductDetailView.as_view(), name='product'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
     path('contacts/', contacts, name='contacts'),
-    path('add_product/', add_product, name='add_product'),
+    path('product_form/', ProductCreateView.as_view(), name='product_form'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from catalog.models import Product, Contacts, Category
 
 
-def index(request):
-
-    content = {
-        'object_list': Product.objects.order_by('-creation_date')[:6],
+class ProductListView(ListView):
+    model = Product
+    extra_context = {
         'title': 'Последние поступления:'
     }
-    return render(request, 'catalog/index.html', content)
+
+    def get_queryset(self):
+        queryset = Product.objects.order_by('-creation_date')[:6]
+        return queryset
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'catalog/products_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['title'] = f'Товары из категории: {category_item.name}'
+        return context_data
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(pk=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = f'Товар из категории: {Product.objects.get(pk=self.kwargs.get("pk"))}'
+        return context_data
+
+
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
+        'title': 'Категории товаров'
+    }
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image_preview', 'price', 'category')
+    success_url = reverse_lazy('catalog:index')
+    extra_context = {
+        'title': 'Добавить новый продукт:'
+    }
 
 
 def contacts(request):
@@ -24,60 +74,4 @@ def contacts(request):
         'title': f'Контактная информация:'
     }
     return render(request, 'catalog/contacts.html', content)
-
-
-def products(request, pk):
-
-    category_item = Category.objects.get(pk=pk)
-
-    content = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': f'Товары из категории: {category_item.name}'
-    }
-    return render(request, 'catalog/products.html', content)
-
-
-def product(request, pk):
-
-    category_item = Product.objects.get(pk=pk)
-
-    content = {
-        'object': Product.objects.get(pk=pk),
-        'title': f'Товар из категории: {category_item.category}'
-    }
-    return render(request, 'catalog/product.html', content)
-
-
-def categories(request):
-    content = {
-        'object_list': Category.objects.all(),
-        'title': 'Категории товаров'
-    }
-    return render(request, 'catalog/categories.html', content)
-
-
-def add_product(request):
-
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
-        image_preview = request.POST.get('image_preview')
-        price = request.POST.get('price')
-        id_category = request.POST.get('category')
-        image_preview.save()
-
-        category = Category.objects.get(pk=id_category)
-        Product.objects.create(
-            name=name,
-            description=message,
-            image_preview=image_preview,
-            category=category,
-            price=price
-        )
-
-    content = {
-        'title': f'Добавить новый продукт:',
-        'object_list': Category.objects.all()
-    }
-    return render(request, 'catalog/add_product.html', content)
 
