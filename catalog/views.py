4972c14e054d8ab5d08d5f676240c7a9a6e06d8a
dariabushueva <bushueva84@yandex.ru>
2023-08-14 21:56:46 +0300

    fixed view.py

--- catalog/views.py
+++ catalog/views.py
@@ -66,21 +66,6 @@ class ProductCreateView(CreateView):
     }
 
 
-#def contacts(request):
-#
-#    if request.method == 'POST':
-#        name = request.POST.get('name')
-#        email = request.POST.get('email')
-#        message = request.POST.get('message')
-#        print(f'{name} ({email}): {message}')
-#
-#    content = {
-#        'all_contacts': Contacts.objects.all(),
-#        'title': f'Контактная информация:'
-#    }
-#    return render(request, 'catalog/contacts.html', content)
-
-
 class ContactFormView(FormView):
     form_class = ContactForm
     template_name = 'catalog/contacts.html'
